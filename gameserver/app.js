4d6e758a3f52e7cc7717f34a9aa07ea473f177e6
Julian Hollmann <julian.hollmann@googlemail.com>
2013-02-08 22:47:05 +0100

    files reformatted

--- gameserver/app.js
+++ gameserver/app.js
@@ -15,82 +15,82 @@ var io = require('socket.io').listen(port + 1);
 var io_client = require('socket.io-client');
 var masterCon = io_client.connect(config.master.host + ':' + config.master.port, {reconnect: true});
 
-masterCon.on('connect', function(socket){
-	console.log('connected to master on port ' + config.master.port);
+masterCon.on('connect', function (socket) {
+    console.log('connected to master on port ' + config.master.port);
 });
 masterCon.emit('register', {
-	id: config.id,
-	name: config.name,
-	port: config.port + 1,
-	host: config.host
+    id: config.id,
+    name: config.name,
+    port: config.port + 1,
+    host: config.host
 });
 
 
 io.set('transports', [
-	'websocket'
+    'websocket'
 ]);
 
 var players = {};
 
 // body cleanup interval
 /*setInterval(function() {
-	for(var playerId in players) {
-		if(players[playerId].lastUpdate + 10000 < new Date().getTime()) {
-			players[playerId].worldPos.x = -100;
-			players[playerId].worldPos.y = -100;
-		}
-	}
-}, 1000);*/
+ for(var playerId in players) {
+ if(players[playerId].lastUpdate + 10000 < new Date().getTime()) {
+ players[playerId].worldPos.x = -100;
+ players[playerId].worldPos.y = -100;
+ }
+ }
+ }, 1000);*/
 
 var barriers = [
-	{
-		worldPos: {
-			x: 500,
-			y: 500,
-			azimuth: 120
-		},
-		type: 1
-	}
+    {
+        worldPos: {
+            x: 500,
+            y: 500,
+            azimuth: 120
+        },
+        type: 1
+    }
 ];
 
-io.sockets.on('connection', function(socket) {
-  socket.emit('currentPlayerData', {
-    playerId: socket.store.id,
-    worldPos: {
-      x: 400,
-      y: 400
-    },
-    rotation: 0
-  });
-  socket.emit('players', players);
-  socket.emit('barriers', barriers);
+io.sockets.on('connection', function (socket) {
+    socket.emit('currentPlayerData', {
+        playerId: socket.store.id,
+        worldPos: {
+            x: 400,
+            y: 400
+        },
+        rotation: 0
+    });
+    socket.emit('players', players);
+    socket.emit('barriers', barriers);
 
-  socket.on('currentPlayerClientUpdate', function(data) {
-    data.lastUpdate = new Date().getTime();
-    players[data.playerId] = data;
-  });
+    socket.on('currentPlayerClientUpdate', function (data) {
+        data.lastUpdate = new Date().getTime();
+        players[data.playerId] = data;
+    });
 
-  setInterval(function() {
-    //socket.emit('players', players);
-  }, 20);
+    setInterval(function () {
+        //socket.emit('players', players);
+    }, 20);
 
 });
 
 var app = express();
 
-app.configure(function() {
-  app.set('port', process.env.PORT || port);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
+app.configure(function () {
+    app.set('port', process.env.PORT || port);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
 });
 
-app.configure('development', function() {
-  app.use(express.errorHandler());
+app.configure('development', function () {
+    app.use(express.errorHandler());
 });
 
-http.createServer(app).listen(config.port, function() {
-  console.log("gameserver listening on port " + config.port);
+http.createServer(app).listen(config.port, function () {
+    console.log("gameserver listening on port " + config.port);
 });
--- masterserver/app.js
+++ masterserver/app.js
@@ -8,37 +8,41 @@ var path = require('path');
 var io = require('socket.io').listen(3001);
 
 var gameServers = {};
+var dummyUser = {
+    email: 'test@test.local',
+    password: 'test'
+};
 
 // enable socket.io to listen for connections from gameservers
 var io_gameserver = require('socket.io').listen(3002);
-io_gameserver.on('connection', function(socket) {
-	socket.on('register', function(data) {
-		gameServers[data.id] = data;
-	});
+io_gameserver.on('connection', function (socket) {
+    socket.on('register', function (data) {
+        gameServers[data.id] = data;
+    });
 });
 
-io.on('connection', function(socket) {
-	setInterval(function() {
-		socket.emit('gameserver', gameServers);
-	}, 500);
+io.on('connection', function (socket) {
+    setInterval(function () {
+        socket.emit('gameserver', gameServers);
+    }, 500);
 });
 
 var app = express();
 
-app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname + '/../', 'client/')));
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname + '/../', 'client/')));
 });
 
-app.configure('development', function() {
-  app.use(express.errorHandler());
+app.configure('development', function () {
+    app.use(express.errorHandler());
 });
 
-http.createServer(app).listen(app.get('port'), function() {
-  console.log("masterserver listening on port " + app.get('port'));
+http.createServer(app).listen(app.get('port'), function () {
+    console.log("masterserver listening on port " + app.get('port'));
 });
